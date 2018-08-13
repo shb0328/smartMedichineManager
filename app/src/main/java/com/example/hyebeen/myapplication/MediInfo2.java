4d200ffbbf1b02ac34de47508739293111897b8f
shb0328 <hyebeen0328@naver.com>
2018-08-14 08:25:20 +0900

    warning 수정 @string 등록 ui 편집 button 객체 추가

--- app/src/main/java/com/example/hyebeen/myapplication/MediInfo2.java
+++ app/src/main/java/com/example/hyebeen/myapplication/MediInfo2.java
@@ -3,16 +3,21 @@ package com.example.hyebeen.myapplication;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MediInfo2 extends AppCompatActivity {
 
-    Button btnHome;
+    Button btnSearch;
     Button btnReg;
 
     protected void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
         setContentView(R.layout.medi2_set);
 
-        //btnReg = (Button)findViewById(R.id.btnReg);
+
+
+        //btnSearch = (Button)findViewById(R.id.);
+
+        //btnReg = (Button)findViewById(R.id.);
     }
 }
