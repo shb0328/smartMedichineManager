4ed1bc383df273aed32df6f1fa6e81edde9c1f80
SHB-0328\hyebeen <shb0328@gmail.com>
2018-08-14 18:43:09 +0900

    test용으로 3번버튼에 검색화면 생성

---
+++ app/src/main/java/com/example/hyebeen/myapplication/MediInfo3.java
@@ -0,0 +1,16 @@
+package com.example.hyebeen.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MediInfo3 extends AppCompatActivity {
+
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.medi3_test_search);
+
+
+
+    }
+
+}
