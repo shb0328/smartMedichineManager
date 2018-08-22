8420808ea5435de2b72fa22ff18e8f12be587bf8
SHB-0328\hyebeen <shb0328@gmail.com>
2018-08-22 18:40:04 +0900

    init

--- app/src/main/java/com/example/hyebeen/myapplication/MediInfo2.java
+++ app/src/main/java/com/example/hyebeen/myapplication/MediInfo2.java
@@ -8,23 +8,15 @@ import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MediInfo2 extends AppCompatActivity {
 
     Button btnSearch;
     Button btnReg;
 
-//    private List<String> list;          // 데이터를 넣은 리스트변수
-//    private ListView nameList;          // 검색을 보여줄 리스트변수
     private EditText textSearch;        // 검색어를 입력할 Input 창
-//    private SearchAdapter adapter;      // 리스트뷰에 연결할 아답터
-//    private ArrayList<String> arraylist;
 
-    private ArrayAdapter<String> adapter2;
+    private ArrayAdapter<String> adapter;
     private AutoCompleteTextView autoCompleteTextView;
     String ex[] = {"apple", "approach", "appa", "apart", "banana"};
 
@@ -32,37 +24,11 @@ public class MediInfo2 extends AppCompatActivity {
         super.onCreate(saveInstanceState);
         setContentView(R.layout.medi2_set);
 
-
-
-        //btnSearch = (Button)findViewById(R.id.);
-
-        //btnReg = (Button)findViewById(R.id.);
-
         textSearch = (EditText) findViewById(R.id.textSearch);
-//        nameList = (ListView) findViewById(R.id.nameList);
         autoCompleteTextView = (AutoCompleteTextView)findViewById(R.id.textSearch);
 
-//        // 리스트를 생성한다.
-//        list = new ArrayList<String>();
-//
-//        // 검색에 사용할 데이터을 미리 저장한다.
-//        settingList();
-//
-//        // 리스트의 모든 데이터를 arraylist에 복사한다.// list 복사본을 만든다.
-//        arraylist = new ArrayList<String>();
-//        arraylist.addAll(list);
-//
-//        // 리스트에 연동될 아답터를 생성한다.
-//        adapter = new SearchAdapter(list, this);
-//
-//        // 리스트뷰에 아답터를 연결한다.
-//        nameList.setAdapter(adapter);
-
-
-
-//        adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,ex);
-        adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,ex);
-        autoCompleteTextView.setAdapter(adapter2);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,ex); //JSON 파일로 받아온 약이름 배열을 연결
+        autoCompleteTextView.setAdapter(adapter);
 
         // input창에 검색어를 입력시 "addTextChangedListener" 이벤트 리스너를 정의한다.
         textSearch.addTextChangedListener(new TextWatcher() {
@@ -78,51 +44,12 @@ public class MediInfo2 extends AppCompatActivity {
             @Override
             public void afterTextChanged(Editable editable) {
                 // input창에 문자를 입력할때마다 호출된다.
-                // search 메소드를 호출한다.
                 String text = autoCompleteTextView.getText().toString();
-//                search(text);
             }
         });
 
     }
 
-//    // 검색을 수행하는 메소드
-//    public void search(String charText) {
-//
-//        // 문자 입력시마다 리스트를 지우고 새로 뿌려준다.
-//        list.clear();
-//
-//        // 문자 입력이 없을때는 모든 데이터를 보여준다.
-//        if (charText.length() == 0) {
-//            list.addAll(arraylist);
-//        }
-//        // 문자 입력을 할때..
-//        else
-//        {
-//            // 리스트의 모든 데이터를 검색한다.
-//            for(int i = 0;i < arraylist.size(); i++)
-//            {
-//                // arraylist의 모든 데이터에 입력받은 단어(charText)가 포함되어 있으면 true를 반환한다.
-//                if (arraylist.get(i).toLowerCase().contains(charText))
-//                {
-//                    // 검색된 데이터를 리스트에 추가한다.
-//                    list.add(arraylist.get(i));
-//                }
-//            }
-//        }
-//        // 리스트 데이터가 변경되었으므로 아답터를 갱신하여 검색된 데이터를 화면에 보여준다.
-//        adapter.notifyDataSetChanged();
-//    }
-
-//    // 검색에 사용될 데이터를 리스트에 추가한다.
-//    private void settingList(){
-//        list.add("apple");
-//        list.add("banana");
-//        list.add("소혜빈");
-//        list.add("신승은");
-//        list.add("박진우");
-//        list.add("김병수");
-//    }
 
 }
 
