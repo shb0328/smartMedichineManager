13ac1d932ee9cf3be59fefc18ce4cc3e9edf0548
SHB-0328\hyebeen <shb0328@gmail.com>
2018-08-25 17:57:02 +0900

    searchMedi 준비

---
+++ app/src/main/java/com/example/hyebeen/myapplication/SearchMedi.java
@@ -0,0 +1,14 @@
+package com.example.hyebeen.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SearchMedi extends AppCompatActivity {
+
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.search_medi);
+
+    }
+
+}
