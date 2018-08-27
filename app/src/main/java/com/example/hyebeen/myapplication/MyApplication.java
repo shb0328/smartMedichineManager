bc52828718f6444c0e22b9af6ec987ef90de8ce8
SHB-0328\hyebeen <shb0328@gmail.com>
2018-08-27 14:26:06 +0900

    realm   init용 application 파일 추가

---
+++ app/src/main/java/com/example/hyebeen/myapplication/MyApplication.java
@@ -0,0 +1,13 @@
+package com.example.hyebeen.myapplication;
+
+import android.app.Application;
+
+import io.realm.Realm;
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Realm.init(this);
+    }
+}
