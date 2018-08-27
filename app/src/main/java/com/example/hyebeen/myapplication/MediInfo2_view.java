6a9a18769dddf97812a6f32894c01805837051a5
SHB-0328\hyebeen <shb0328@gmail.com>
2018-08-27 13:57:43 +0900

    medi2_info 액티비티 및 레이아웃 추가

---
+++ app/src/main/java/com/example/hyebeen/myapplication/MediInfo2_view.java
@@ -0,0 +1,13 @@
+package com.example.hyebeen.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MediInfo2_view extends AppCompatActivity {
+
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.medi1_info);
+
+    }
+}
