797a8ff9986a74ca5419ad2b38b65523542b0883
SHB-0328\hyebeen <shb0328@gmail.com>
2018-08-27 17:37:39 +0900

    일단

--- app/src/main/java/com/example/hyebeen/myapplication/MainActivity.java
+++ app/src/main/java/com/example/hyebeen/myapplication/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import io.realm.Realm;
+
 public class MainActivity extends AppCompatActivity {
 
     Button b1;
@@ -14,22 +16,45 @@ public class MainActivity extends AppCompatActivity {
     Button b3;
     Button b4;
 
+    //localDB
+    private Realm realm;
+    private MediDBControler mediDBControler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        realm = Realm.getDefaultInstance(); //사용준비
+        mediDBControler = new MediDBControler();
+
+        mediDBControler.createMediDB(realm);
+
+
+
+        //button
         b1 = (Button) findViewById(R.id.button1);
         b1.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
 
-                //TODO:등록된 정보가 있다면,
-                Intent intent1_o = new Intent(getApplicationContext(), MediInfo1.class);
-                startActivity(intent1_o);
+                if(mediDBControler.getMediData_num1(realm) == null){
+                    //TODO:등록된 정보가 없다면,
+                    Toast.makeText(getApplicationContext(),
+                            "등록된 약 정보가 없습니다.\n새로운 약을 등록해주세요.", Toast.LENGTH_LONG).show();
 
-                //TODO:등록된 정보가 없다면,
+                    Intent intent_x = new Intent(getApplicationContext(), Medi_setting.class);
+                    intent_x.putExtra("MediData",mediDBControler.getMediData_num1(realm)); //null
+                    startActivity(intent_x);
+                }
+                else {
+                    //TODO:등록된 정보가 있다면,
+                    Intent intent_o = new Intent(getApplicationContext(), MediInfo1.class);
+                    intent_o.putExtra("MediData",mediDBControler.getMediData_num1(realm)); //db존재
+                    startActivity(intent_o);
+                }
+
             }
         });
 
@@ -39,16 +64,82 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                //TODO:등록된 정보가 있다면,
+                if(mediDBControler.getMediData_num2(realm) == null){
+                    //TODO:등록된 정보가 없다면,
+                    Toast.makeText(getApplicationContext(),
+                            "등록된 약 정보가 없습니다.\n새로운 약을 등록해주세요.", Toast.LENGTH_LONG).show();
 
+                    Intent intent_x = new Intent(getApplicationContext(), Medi_setting.class);
+                    intent_x.putExtra("MediData",mediDBControler.getMediData_num2(realm)); //null
+                    startActivity(intent_x);
+                }
+                else {
+                    //TODO:등록된 정보가 있다면,
+                    Intent intent_o = new Intent(getApplicationContext(), MediInfo2.class);
+                    intent_o.putExtra("MediData",mediDBControler.getMediData_num2(realm)); //db존재
+                    startActivity(intent_o);
+                }
 
-                //TODO:등록된 정보가 없다면,
-                Toast.makeText(getApplicationContext(),
-                        "등록된 약 정보가 없습니다.\n새로운 약을 등록해주세요.", Toast.LENGTH_LONG).show();
-                Intent intent1_x = new Intent(getApplicationContext(), MediInfo2_setting.class);
-                startActivity(intent1_x);
             }
         });
 
+        b3 = (Button) findViewById(R.id.button3);
+        b3.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                if(mediDBControler.getMediData_num3(realm) == null){
+                    //TODO:등록된 정보가 없다면,
+                    Toast.makeText(getApplicationContext(),
+                            "등록된 약 정보가 없습니다.\n새로운 약을 등록해주세요.", Toast.LENGTH_LONG).show();
+
+                    Intent intent_x = new Intent(getApplicationContext(), Medi_setting.class);
+                    intent_x.putExtra("MediData",mediDBControler.getMediData_num3(realm)); //null
+                    startActivity(intent_x);
+                }
+                else {
+                    //TODO:등록된 정보가 있다면,
+//                    Intent intent_o = new Intent(getApplicationContext(), MediInfo3.class);
+//                    intent_o.putExtra("MediData",mediDBControler.getMediData_num3(realm)); //db존재
+//                    startActivity(intent_o);
+                }
+
+            }
+        });
+
+        b4 = (Button) findViewById(R.id.button4);
+        b4.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                if(mediDBControler.getMediData_num4(realm) == null){
+                    //TODO:등록된 정보가 없다면,
+                    Toast.makeText(getApplicationContext(),
+                            "등록된 약 정보가 없습니다.\n새로운 약을 등록해주세요.", Toast.LENGTH_LONG).show();
+
+                    Intent intent_x = new Intent(getApplicationContext(), Medi_setting.class);
+                    intent_x.putExtra("MediData",mediDBControler.getMediData_num4(realm)); //null
+                    startActivity(intent_x);
+                }
+                else {
+                    //TODO:등록된 정보가 있다면,
+//                    Intent intent_o = new Intent(getApplicationContext(), MediInfo4.class);
+//                    intent_o.putExtra("MediData",mediDBControler.getMediData_num4(realm)); //db존재
+//                    startActivity(intent_o);
+                }
+
+            }
+        });
+
+
+    }
+
+    //realm 해제 (메모리누수방지)
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        realm.close();
     }
 }
