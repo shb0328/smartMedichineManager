e82214589cc75e10ce5e6f2beb4a394d35b5e716
SHB-0328\hyebeen <shb0328@gmail.com>
2018-08-28 18:39:51 +0900

    clear

--- app/src/main/java/com/example/hyebeen/myapplication/MediInfo2.java
+++ app/src/main/java/com/example/hyebeen/myapplication/MediInfo2.java
@@ -3,6 +3,7 @@ package com.example.hyebeen.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -42,6 +43,12 @@ public class MediInfo2 extends AppCompatActivity {
         one = (TextView) findViewById(R.id.one);
         all = (TextView) findViewById(R.id.all);
         resetButton = (ImageButton) findViewById(R.id.resetButton);
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediDataControler.clear(realm);
+            }
+        });
 
 //        RealmResults<MediData> mediData = realm.where(MediData.class)
 //                .equalTo("num", 2)
