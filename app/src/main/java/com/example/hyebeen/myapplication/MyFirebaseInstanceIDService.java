9ea2a75171d8ace40b83333c7d40516bbd1c0513
SHB-0328\hyebeen <shb0328@gmail.com>
2018-10-08 13:07:51 +0900

    MyFirebaseInstanceIDService.java 파일 추가

---
+++ app/src/main/java/com/example/hyebeen/myapplication/MyFirebaseInstanceIDService.java
@@ -0,0 +1,41 @@
+package com.example.hyebeen.myapplication;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import java.io.IOException;
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+    private static final String TAG = "MyFirebaseIDService";
+    private static final String myURL = "";
+    @Override
+    public void onTokenRefresh(){
+        String token = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG,"Refreshed token"+token);
+
+        sendRegistrationToServer(token);
+    }
+
+    private void sendRegistrationToServer(String token) {
+        /**
+         ** Add custom implementation, as needed
+         **/
+
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = new FormBody.Builder().add("Token",token).build();
+
+        //request
+        Request request = new Request.Builder()
+                .url("http://"+myURL+"/api/fcmtest")
+                .post(body)
+                .build();
+
+        try{
+            client.newCall(request).execute();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
