e52a05fd0e40b98fa30caea3813b0ab988ac63a4
SHB-0328\hyebeen <shb0328@gmail.com>
2018-10-10 20:46:36 +0900

    몰라... 채널을 만들어야한다...

--- app/src/main/java/com/example/hyebeen/myapplication/AlarmNotificationService.java
+++ app/src/main/java/com/example/hyebeen/myapplication/AlarmNotificationService.java
@@ -1,10 +1,13 @@
 package com.example.hyebeen.myapplication;
 
+import android.annotation.TargetApi;
 import android.app.IntentService;
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
 
@@ -25,6 +28,7 @@ public class AlarmNotificationService extends IntentService {
     }
 
     //handle notification
+    @TargetApi(Build.VERSION_CODES.O)
     private void sendNotification(String msg) {
         alarmNotificationManager = (NotificationManager) this
                 .getSystemService(Context.NOTIFICATION_SERVICE);
@@ -34,11 +38,17 @@ public class AlarmNotificationService extends IntentService {
                 new Intent(this, MainActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);
 
         //Create notification
-        NotificationCompat.Builder alamNotificationBuilder = new NotificationCompat.Builder(
-                this).setContentTitle("Alarm").setSmallIcon(R.mipmap.ic_launcher)
-                .setStyle(new NotificationCompat.BigTextStyle().bigText(msg))
-                .setContentText(msg).setAutoCancel(true);
-        alamNotificationBuilder.setContentIntent(contentIntent);
+//        NotificationCompat.Builder alamNotificationBuilder = new NotificationCompat.Builder(
+//                this).setContentTitle("Alarm").setSmallIcon(R.mipmap.ic_launcher)
+//                .setStyle(new NotificationCompat.BigTextStyle().bigText(msg))
+//                .setContentText(msg).setAutoCancel(true);
+//        alamNotificationBuilder.setContentIntent(contentIntent);
+
+        Notification.Builder builder = new Notification.Builder(this)
+                .setContentTitle("title")
+                .setContentText("body")
+                .setSmallIcon()
+                .setAutoCancel(true);
 
         //notiy notification manager about new notification
         alarmNotificationManager.notify(NOTIFICATION_ID, alamNotificationBuilder.build());
