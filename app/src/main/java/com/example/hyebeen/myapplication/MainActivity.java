30fb522a2ab5447eceebf77df4b8510caf78a421
SHB-0328\hyebeen <shb0328@gmail.com>
2018-10-21 11:54:09 +0900

    네이버 의약검색 연동 성공

--- app/src/main/java/com/example/hyebeen/myapplication/MainActivity.java
+++ app/src/main/java/com/example/hyebeen/myapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.hyebeen.myapplication;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     Button b3;
     Button b4;
     ImageButton Location;
+    ImageButton naverBtn;
 
     //localDB
 
@@ -149,6 +151,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(getApplicationContext(),PharmacyLocation.class);
+                startActivity(intent);
+            }
+        });
+
+        naverBtn = (ImageButton)findViewById(R.id.naverBtn);
+        naverBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://terms.naver.com/medicineSearch.nhn"));
                 startActivity(intent);
             }
         });
